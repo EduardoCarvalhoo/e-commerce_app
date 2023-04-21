683775f86d0ca85144775a4e262299f54f9cc948
EduardoCarvalhoo <eduardo-carvalho11@hotmail.com>
2023-04-21 18:08:11 -0300

    feat: creating the splash screen and implementing the first components

---
+++ app/src/main/java/com/example/e_commerce_app/presentation/home/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.e_commerce_app.presentation.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.e_commerce_app.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
