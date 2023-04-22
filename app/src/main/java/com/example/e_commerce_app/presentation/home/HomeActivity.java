0d5d1e6c5e1ba58a7f854312d7439060963760af
EduardoCarvalhoo <eduardo-carvalho11@hotmail.com>
2023-04-22 13:38:43 -0300

    feat: defining view binding and adding internet access

--- app/src/main/java/com/example/e_commerce_app/presentation/home/HomeActivity.java
+++ app/src/main/java/com/example/e_commerce_app/presentation/home/HomeActivity.java
@@ -3,14 +3,15 @@ package com.example.e_commerce_app.presentation.home;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
-import com.example.e_commerce_app.R;
+import com.example.e_commerce_app.databinding.ActivityHomeBinding;
 
 public class HomeActivity extends AppCompatActivity {
+    private ActivityHomeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
