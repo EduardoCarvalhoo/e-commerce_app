4de0e10214252f01df4384b13f2fdf2f7937998f
EduardoCarvalhoo <eduardo-carvalho11@hotmail.com>
2024-02-11 10:10:24 -0300

    feat: applying splash screen improvements

--- app/src/main/java/com/example/e_commerce_app/presentation/home/splashScreen/InitialPresentationActivity.java
+++ app/src/main/java/com/example/e_commerce_app/presentation/home/splashScreen/InitialPresentationActivity.java
@@ -3,9 +3,12 @@ package com.example.e_commerce_app.presentation.home.splashScreen;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.e_commerce_app.R;
 import com.example.e_commerce_app.databinding.ActivityInitialPresentationBinding;
 import com.example.e_commerce_app.presentation.home.activity.HomeActivity;
 
@@ -22,6 +25,9 @@ public class InitialPresentationActivity extends AppCompatActivity {
     }
 
     public void setupSplashScreen() {
+        Animation slideAnimation = AnimationUtils.loadAnimation(this, R.anim.side_slide);
+        binding.initialPresentationLogoSplashScreen.startAnimation(slideAnimation);
+
         final Handler handler = new Handler();
         handler.postDelayed(() -> {
             Intent intent = new Intent(InitialPresentationActivity.this, HomeActivity.class);
